3b88eada1597cae6e3569256f43c145c40411347
rungnapa <rungnapa.su.61@ubu.ac.th>
2020-12-14 10:06:38 +0700

    ADD

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
-import 'package:appFlutter/favoriteA.dart';
-import 'package:appFlutter/favoriteB.dart';
-import 'package:appFlutter/favoriteC.dart';
+
 import 'package:flutter/material.dart';
 // Uncomment lines 7 and 10 to view the visual layout at runtime.
 // import 'package:flutter/rendering.dart' show debugPaintSizeEnabled;
@@ -42,7 +40,11 @@ class MyApp extends StatelessWidget {
             ),
           ),
           /*3*/
-          FavoriteWidget(),
+          Icon(
+            Icons.star,
+            color: Colors.red[500],
+          ),
+          Text('41'),
         ],
       ),
     );
@@ -60,25 +62,15 @@ class MyApp extends StatelessWidget {
       ),
     );
 
-    Widget favoriteSection = Row(
-      mainAxisAlignment: MainAxisAlignment.spaceAround,
-      children: [
-        TapboxA(),
-        ParentWidgetB(),
-        ParentWidgetC(),
-      ],
-    );
-
-
     Widget textSection = Container(
       padding: const EdgeInsets.all(32),
       child: Text(
         'Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese '
-            'Alps. Situated 1,578 meters above sea level, it is one of the '
-            'larger Alpine Lakes. A gondola ride from Kandersteg, followed by a '
-            'half-hour walk through pastures and pine forest, leads you to the '
-            'lake, which warms to 20 degrees Celsius in the summer. Activities '
-            'enjoyed here include rowing, and riding the summer toboggan run.',
+        'Alps. Situated 1,578 meters above sea level, it is one of the '
+        'larger Alpine Lakes. A gondola ride from Kandersteg, followed by a '
+        'half-hour walk through pastures and pine forest, leads you to the '
+        'lake, which warms to 20 degrees Celsius in the summer. Activities '
+        'enjoyed here include rowing, and riding the summer toboggan run.',
         softWrap: true,
       ),
     );
@@ -99,9 +91,7 @@ class MyApp extends StatelessWidget {
             ),
             titleSection,
             buttonSection,
-            favoriteSection,
             textSection,
-
           ],
         ),
       ),
@@ -123,50 +113,6 @@ class MyApp extends StatelessWidget {
               fontWeight: FontWeight.w400,
               color: color,
             ),
-          ),
-        ),
-      ],
-    );
-  }
-}
-
-class FavoriteWidget extends StatefulWidget {
-  @override
-  _FavoriteWidgetState createState() => _FavoriteWidgetState();
-}
-class _FavoriteWidgetState extends State<FavoriteWidget> {
-  bool _isFavorited = true;
-  int _favoriteCount = 41;
-  void _toggleFavorite() {
-  setState(() {
-    if (_isFavorited) {
-      _favoriteCount -= 1;
-      _isFavorited = false;
-    } else {
-      _favoriteCount += 1;
-      _isFavorited = true;
-    }
-  });
-}
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        Container(
-          padding: EdgeInsets.all(0),
-          child: IconButton(
-            padding: EdgeInsets.all(0),
-            alignment: Alignment.centerRight,
-            icon: (_isFavorited ? Icon(Icons.star) : Icon(Icons.star_border)),
-            color: Colors.red[500],
-            onPressed: _toggleFavorite,
-          ),
-        ),
-        SizedBox(
-          width: 18,
-          child: Container(
-            child: Text('$_favoriteCount'),
           ),
         ),
       ],
